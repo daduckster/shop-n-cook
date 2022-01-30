dd69055d9785553b7a3fc99d8ef6ede233e8afcf
daduckster <vlasak.o@yandex.ru>
2022-01-30 14:35:51 +0100

    working on recipeDom function

--- src/modules/your-recipes/recipeItem.js
+++ src/modules/your-recipes/recipeItem.js
@@ -1,8 +1,12 @@
+import { recipesInStorage } from '../new-recipe/newRecipeElements.js';
+
 const recipesContainer = document.querySelectorAll('.main-recipes__recipes-container');
 
 const createRecipeDOM = () => {
 	const recipeDetails = document.createElement('details');
 	const recipeSummary = document.createElement('summary');
+	const recipeSummaryP = document.createElement('p');
+	const recipeSummaryImg = document.createElement('img');
 
 	const ingredientsForm = document.createElement('form');
 	const ingredientsHeader = document.createElement('header');
@@ -16,4 +20,25 @@ const createRecipeDOM = () => {
 	const recipeBtnContainer = document.createElement('div');
 	const recipeSendToTopBtn = document.createElement('button');
 	const recipeEditBtn = document.createElement('button');
+
+	// CLASSES ////////////////////////////////////////////////
+
+	recipeDetails.classList.add('recipe');
+	recipeSummaryP.classList.add('section-heading');
+	recipeSummaryImg.classList.add('dropdown--closed');
+
+	ingredientsForm.classList.add('recipe__ingredients');
+
+	instructionsSection.classList.add('recipe__instructions');
+
+	recipeBtnContainer.classList.add('recipe__buttons');
+	recipeSendToTopBtn.classList.add('btn');
+	recipeSendToTopBtn.classList.add('btn--send-to-top');
+	recipeEditBtn.classList.add('recipe__buttons__link');
+
+	// CONTENTS ////////////////////////////////////////////////
+
+	recipeSummaryP.textContent = `${recipe.name}`;
+
+	ingredientsHeader.textContent = 'Ingredients:';
 };
